2722681d4b9d94b85f6b9d2d072903f7d20e698d
rinkunnn-hp <75443958+rinkunnn-hp@users.noreply.github.com>
2020-12-31 03:39:17 +0900

    Update index.php

--- 友達サーバー用サイト/初参加用/index.php
+++ 友達サーバー用サイト/初参加用/index.php
@@ -57,7 +57,7 @@ if(isset($_POST['discord'])) {
 session_start();
  
 //ログイン済みかを確認
-if (!isset($_SESSION['USER'])) {
+if (!isset($_SESSION['USER-f'])) {
     header('Location: /友達サーバー用サイト/ログイン');
     exit;
 }
