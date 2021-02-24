3b7eaf37b55f0422b0124b9e65c9b85f952a8f71
nikutaro <78470099+nikutaro@users.noreply.github.com>
2021-02-24 16:50:42 +0900

    Update index.php

--- きゅうり/クイズ/index.php
+++ きゅうり/クイズ/index.php
@@ -3,4 +3,7 @@
   <php
         if(isset($_POST['start'])){
            echo("問題1 パンはパンでも食べられないパンはなーんだ");
+           answer = 1
+       if(isset($_POST['start']) && anser == 1){
+           echo("答え
        ?>
