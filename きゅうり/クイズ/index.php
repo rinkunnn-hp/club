53d29e01ce8534483d9e8d2a8f667cda3703a5cc
nikutaro <78470099+nikutaro@users.noreply.github.com>
2021-03-03 16:57:35 +0900

    Create index.php

--- きゅうり/クイズ/index.php
+++ きゅうり/クイズ/index.php
@@ -1,4 +1,9 @@
-<form action="./index.php" method="post" enctype="multipart/form-data">
+<form action="index.php" method="post" enctype="multipart/form-data">
+<input type="submit" id=start name=start value=START>
   <?php
-    echo ("問題1 パンはパンでも食べられないパンはなーんだ");
-       ?>
+        if(isset($_POST['start'])){
+           echo"問題1 織田信長が自害した出来事を何というか？";
+           }
+  ?>
+      
+
