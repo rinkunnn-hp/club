2d18994f5963fd0f46f532110a4253e8bdf3cded
nikutaro <78470099+nikutaro@users.noreply.github.com>
2021-03-03 17:04:11 +0900

    Update index.php

--- きゅうり/クイズ/index.php
+++ きゅうり/クイズ/index.php
@@ -5,7 +5,7 @@
            echo"問題1 織田信長が自害した出来事を何というか？";
            echo"<input type=submit id=a1 name=a1 value=寺>";
            if(isset($_POST['a1'])){
-             echo"あったりー";
+              echo"あったりー";
            }
         }
   ?>
