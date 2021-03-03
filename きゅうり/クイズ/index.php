beb24ad66867e75456a0e25d61de237304d7147d
nikutaro <78470099+nikutaro@users.noreply.github.com>
2021-03-03 16:50:56 +0900

    Update index.php

--- きゅうり/クイズ/index.php
+++ きゅうり/クイズ/index.php
@@ -1,8 +1,5 @@
 <form action="index.php" method="post" enctype="multipart/form-data">
 <input type="submit" id=start name=start value=START>
   <php
-        if(isset($_POST['start'])){
-           echo"問題1 パンはパンでも食べられないパンはなーんだ";
-           }
-echo"問題1 パンはパンでも食べられないパンはなーんだ";
+    echo ("問題1 パンはパンでも食べられないパンはなーんだ");
        ?>
