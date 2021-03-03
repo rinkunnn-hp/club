d6ae2062f61bf4c13321bfd678dfa93fd04662fc
nikutaro <78470099+nikutaro@users.noreply.github.com>
2021-03-03 16:43:26 +0900

    Update index.php

--- きゅうり/クイズ/index.php
+++ きゅうり/クイズ/index.php
@@ -2,6 +2,6 @@
 <input type="submit" id=start name=start value=START>
   <php
         if(isset($_POST['start'])){
-           echo("問題1 パンはパンでも食べられないパンはなーんだ");
+           echo"問題1 パンはパンでも食べられないパンはなーんだ";
            }
        ?>
