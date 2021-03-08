7f518322dda727aa8ad3a0f4e1774ef55194c737
nikutaro <78470099+nikutaro@users.noreply.github.com>
2021-03-08 17:00:22 +0900

    Update index.php

--- きゅうり/クイズ/index.php
+++ きゅうり/クイズ/index.php
@@ -1,8 +1,7 @@
 <form action="index.php" method="post" enctype="multipart/form-data">
 <input type="submit" id=start name=start value=START>
  
-
-  <?php
+　 <?php
         if(isset($_POST['start'])){
            echo"問題1 織田信長が自害した出来事を何というか？";
            echo"<input type=submit id=a1-1 name=a1-1 value=寺>";
